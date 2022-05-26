73d14ebe0e587166609a34ca53cb3425b3245607
ististyle <53050102+ististyle@users.noreply.github.com>
2022-05-27 01:12:55 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
-let color = '#3aa757';
+/*let color = '#3aa757';*/
 let probability = '0.1';
 let revert = '0'
 let advanced = '1'
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  /*chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);*/
   chrome.storage.sync.set({ probability });
   chrome.storage.sync.set({ revert });
   chrome.storage.sync.set({ advanced });
